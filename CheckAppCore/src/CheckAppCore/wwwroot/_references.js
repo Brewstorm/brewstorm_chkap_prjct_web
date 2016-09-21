f4fe8d4abf178bc507c39776bda733b4d52562aa
andermeurer <3eKSAt7qj0A8>
2016-09-21 01:20:23 -0300

    Página de login funcional utilizando cookies.

--- CheckAppCore/src/CheckAppCore/wwwroot/_references.js
+++ CheckAppCore/src/CheckAppCore/wwwroot/_references.js
@@ -6,4 +6,5 @@
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/promise-polyfill/Promise.js" />
+/// <reference path="lib/web-animations-js/web-animations.min.js" />
 /// <reference path="lib/webcomponentsjs/webcomponents.js" />
